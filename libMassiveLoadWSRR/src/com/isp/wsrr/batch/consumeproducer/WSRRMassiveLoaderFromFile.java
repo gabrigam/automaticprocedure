3c5060e510e8d847c46373807642d766dba79a60
gabrigam <gabrigam@gmail.com>
2018-07-05 17:31:05 +0200

    Started ...Batch Massive Loader WSRR SCHOST -  V1.1 July 2018

--- libMassiveLoadWSRR/src/com/isp/wsrr/batch/consumeproducer/WSRRMassiveLoaderFromFile.java
+++ libMassiveLoadWSRR/src/com/isp/wsrr/batch/consumeproducer/WSRRMassiveLoaderFromFile.java
@@ -19,9 +19,10 @@ import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.json.JSONArray;
 
+import com.google.gson.Gson;
 import com.ibm.bancaintesa.connection.Rest;
 import com.isp.wsrr.envelopes.WSRREnvelopes;
-import com.isp.wsrr.utility.WSRRUtility;
+//import com.isp.wsrr.utility.WSRRUtility; tolto
 
 /**
  * 
@@ -57,28 +58,52 @@ public class WSRRMassiveLoaderFromFile {
 			updateLogger(logFileName, "caricamentiMassiviISPAppender",
 					"com.isp.wsrr.batch.consumeproducer.WSRRMassiveLoaderFromFile");
 
-		nbplog.info(
-				"----------------------------------------------------------------------------------------------------------------------");
-		nbplog.info("Started ...Batch Massive Loader WSRR SHOST - (BC) V1.0 Luglio 2017");
-		nbplog.info("URL " + args[0]);
-		nbplog.info("File Name " + args[1]);
-		nbplog.info(
-				"----------------------------------------------------------------------------------------------------------------------");
+
 
 		// check Input parameters
-		if (args.length == 0) {
+		if (args.length != 10) {
 
 			nbplog.info(
 					"----------------------------------------------------------------------------------------------------------------------");
-			nbplog.info("Error insert : URLBPM(0) FileInput(1)  user(2) password(3) ");
+			nbplog.info("Error insert : URLBPM(0) FileInput(1)  operation(2) subtype(3) userbpm(4) password(5) urlWSRR(6) userwsrr(7) password(8) simula(9)");
 			nbplog.info(
 					"----------------------------------------------------------------------------------------------------------------------");
 			Runtime.getRuntime().exit(0); // brutale :)
 
 		}
+		
+		if (args[2]==null || (!args[2].equals("INSERT") && !args[2].equals("NEWVERSION"))) {
+			nbplog.info("Invalid operation use INSERT or NEWVERSION");
+			Runtime.getRuntime().exit(0);
+		}
+		
+		if (args[3]==null || (!args[3].equals("BS") && !args[2].equals("MPE"))) {
+			nbplog.info("Invalid subtype use BS or MPE");
+			Runtime.getRuntime().exit(0);
+		}
 
+		nbplog.info(
+				"----------------------------------------------------------------------------------------------------------------------");
+		nbplog.info("Started ...Batch Massive Loader WSRR SCHOST -  V1.1 July 2018");
+		nbplog.info("URL " + args[0]);
+		nbplog.info("File Name " + args[1]);
+		nbplog.info("Operation " + args[2]);
+		nbplog.info("SubType " + args[3]);
+		nbplog.info("WSRR URL " + args[6]);
+		if (args[9].equals("1")) nbplog.info("Richiesta la SIMULAZIONE NON saranno creati Censimenti!!");
+		nbplog.info(
+				"----------------------------------------------------------------------------------------------------------------------");
+		
+		if (args[9].equals("0")) {
+		nbplog.info(" :-))  Service Created");
+		nbplog.info(" :-((  Service Not Created");
+		}
+		nbplog.info(
+				"----------------------------------------------------------------------------------------------------------------------");		
+		//IXPG0CM@HS_Servizio_Caricamento_Massivo_Tipologia_SCHOST
+		
 		String action = null;
-		String serviceName = "IXPG0%40HS_Servizio_Caricamento_Massivo_SHOST";
+		String serviceName = "IXPG0CM%40HS_Servizio_Caricamento_Massivo_Tipologia_SCHOST";
 		String action_ = args[0] + "/rest/bpm/wle/v1/service/" + serviceName + "?action=start&params=";
 		HashMap headerMap = new HashMap();
 		headerMap.put("Content-Type", "application/json ;  charset=UTF-8");
@@ -95,27 +120,45 @@ public class WSRRMassiveLoaderFromFile {
 
 				try {
 
-					String[] input = line.split(";");
-
-					if (input[0].length() != 0 && input[1].length() != 0 && input[3].length() != 0
-							&& input[4].length() != 0 && input[5].length() != 0) {
-
+					//line=line+"MIGR";
+					
+					String[] input = line.split("@#@");
+									
+					if (input.length ==30) {
+					
+				    //check doc analisi tecnica e funzionale
+						
+				    if (input[4].contains("\\")) {
+				    	
+				    nbplog.error(":(  Exception for Record # " + recNum+ " Analisi Funzionale contiene il campo \\");
+				    	
+				    }
+				    
+				    if (input[5].contains(" ")) {
+				    	
+				    nbplog.error(":(  Exception for Record # " + recNum+ " Analisi Tecnica contiene il campo \\");
+				    	
+				    }
+					
+					String json = new Gson().toJson(input);
+					
+					String jsonBO="{\"operazione\":\"%operazione%\",\"tipoSottoservizio\":\"%sottotipo%\",\"InputDataSCHOST\":";
+					
+					jsonBO=jsonBO+json+",\"urlwsrr\":\"%urlwsrr%\",\"user\":\"%user%\",\"password\":\"%password%\",\"simula\":\"%simula%\"}";
+										
+					jsonBO=jsonBO.replaceAll("%operazione%", args[2]);
+					
+					jsonBO=jsonBO.replaceAll("%sottotipo%", args[3]);
+					
+					jsonBO=jsonBO.replaceAll("%urlwsrr%", args[6]);
+					
+					jsonBO=jsonBO.replaceAll("%user%", args[7]);
+					
+					jsonBO=jsonBO.replaceAll("%password%", args[8]);
+					
+					jsonBO=jsonBO.replaceAll("%simula%", args[9]);
+					
 						nbplog.info("Record # " + recNum + ": " + line);
-
-						String jsonBO = "{\"DatiRichiestiSHOST\":{\"NOME\":\"%NOME%\",\"ACRONIMO\":\"%ACRONIMO%\",\"SSA\":\"%SSA%\",\"DESCRIZIONE\":\"%DESCRIZIONE%\",\"DESCRIZIONE_ESTESA\":\"%DESCRIZIONE_ESTESA%\",\"PGM_SERVIZIO\":\"%PGM_SERVIZIO%\",\"TRANS_SERVIZIO\":\"%TRANS_SERVIZIO%\"}}";
-
-						String ssa = input[2];
-
-						if (ssa.length() == 0)
-							ssa = input[1].substring(0, 2);
-
-						jsonBO = jsonBO.replace("%NOME%", input[0]);
-						jsonBO = jsonBO.replace("%ACRONIMO%", input[1]);
-						jsonBO = jsonBO.replace("%SSA%", ssa);
-						jsonBO = jsonBO.replace("%DESCRIZIONE%", input[3]);
-						jsonBO = jsonBO.replace("%DESCRIZIONE_ESTESA%", input[3]);
-						jsonBO = jsonBO.replace("%PGM_SERVIZIO%", input[4]);
-						jsonBO = jsonBO.replace("%TRANS_SERVIZIO%", input[5]);
 
 						jsonBO = URLEncoder.encode(jsonBO, "UTF-8");
 
@@ -124,28 +167,42 @@ public class WSRRMassiveLoaderFromFile {
 						HashMap responseMap = null;
 
 						try {
-							responseMap = Rest.doRest("POST", action, "", headerMap, args[2], args[3], true, -1);
+							responseMap = Rest.doRest("POST", action, "", headerMap, args[4], args[5], true, -1);
 
 							if (responseMap != null) {
-								nbplog.info("	Result for Record # " + recNum + "---> " + responseMap.get(keycodeMessage)
+
+								String face=":-((";
+								
+								if (!args[9].equals("0")) face="???";
+								
+								if (((String)responseMap.get(keyResponseMessage)).indexOf("Creazione Eseguita Correttamente")!= -1) {
+								    face=":-))";
+									ok++;
+								}
+								
+								if (((String)responseMap.get(keyResponseMessage)).indexOf("Creazione per Nuova Versione Eseguita Correttamente")!= -1) {
+								    face=":-))";
+									ok++;
+								}
+								
+								nbplog.info(face+"   Result for Record # " + recNum + "---> " + responseMap.get(keycodeMessage)
 										+ " - " + responseMap.get(keyResponseMessage) + " - "
 										+ responseMap.get(keyErrorMessage));
 								//Creazione Eseguita Correttamente 
 								
-								if (((String)responseMap.get(keyResponseMessage)).indexOf("Creazione Eseguita Correttamente")!= -1) {
-									ok++;
-								}
+
 							}
 						} catch (Exception ex) {
-							nbplog.error("Exception for Record # " + recNum);
+							nbplog.error(":(  Exception for Record # " + recNum);
+							nbplog.error(ex.getMessage());
 							ex.printStackTrace();
 						}
 
 					} else {
-						nbplog.error("Record # " + recNum + " contains invalid data");
+						nbplog.error(":(  Record # " + recNum + " contains invalid data found : "+input.length+" fields instead 30!");
 					}
 				} catch (Exception ex) {	
-					nbplog.error("Record # " + recNum + " RunTimeError : " +ex.toString());
+					nbplog.error(":(  Record # " + recNum + " RunTimeError : " +ex.toString());
 					ex.printStackTrace();
 				}
 				recNum++;
@@ -161,11 +218,14 @@ public class WSRRMassiveLoaderFromFile {
 				"----------------------------------------------------------------------------------------------------------------------");
 		int ko=(recNum-1-ok);
 		nbplog.info("Total Record/s : "+(recNum-1)) ;
+		if (args[9].equals("0")){
 		nbplog.info("Total Object/s Created : "+ok );
-		nbplog.info("Total Record/s in Error (and Not Created) : "+ko);
+		nbplog.info("Total Record/s in Error/Existing : "+ko);
+		}
 		nbplog.info(
 				"----------------------------------------------------------------------------------------------------------------------");
-		nbplog.info("Terminated ...Batch Massive Loader WSRR SHOST - (BC) V1.0 Luglio 2017... CS");
+		nbplog.info("Terminated ...Batch Massive Loader WSRR SCHOST");
+		nbplog.info("CS");
 	}
 
 	static void updateLogger(String file_name, String appender_name, String package_name) {
